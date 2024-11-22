59a032ce4e19583b62640e58c0c3ace673a764d6
Dimitrije Dimitrijevic <me@dimitrijedimitrijevic.com>
2024-11-22 21:09:06 +0100

    remove IO.inspect

--- lib/test_endpoint.ex
+++ lib/test_endpoint.ex
@@ -2,10 +2,10 @@ defmodule TestEndpoint do
   use Plug.Builder
 
   def init(opts) do
-    IO.inspect(opts)
+    opts
   end
 
-  def call(conn, opts \\ []) do
+  def call(conn, _opts \\ []) do
     websocket = Websocket.init_state(conn, self())
     # Inspect the headers and path before upgrading to websocket
     # Track if IP is alredy connected, if it is we won't  allow more connections from same, as it might be a ddos attack
